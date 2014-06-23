78a59fbe63278b9a35e6581cd281db0040a78ef3
Preston Timmons <prestontimmons@gfa.org>
2014-06-23 21:08:03 +0000

    Updated version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-formtags",
-    version="1.0",
+    version="2.0",
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
