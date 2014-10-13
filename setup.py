2a10e67611d99f15ca767e45afe88cca1464b724
Preston Timmons <prestontimmons@gmail.com>
2014-10-13 18:37:16 -0500

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from setuptools import setup, find_packages
 
+DESCRIPTION = """
+Provides template tags that simplify and enhance form rendering in Django
+from within your template.
+
+https://github.com/prestontimmons/djformtags
+"""
+
 setup(
-    name="Djformtags",
-    version="2.0",
-    long_description=__doc__,
+    name="djformtags",
+    version="1.0",
+    author="Preston Timmons",
+    author_email="prestontimmons@gmail.com",
+    url="https://github.com/prestontimmons/djformtags",
+    description="Simplifies and enhance form rendering in Django templates.",
+    long_description=DESCRIPTION,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
